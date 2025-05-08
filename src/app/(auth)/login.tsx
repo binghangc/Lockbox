de2b806070964a19cce5634c0f1e7fdc975739a4
Emilia Ayu <emiliaayu944@gmail.com>
2025-05-09 00:51:01 +0800

    Update profile page

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -13,7 +13,7 @@ export default function Login() {
 
         try {
             // change it to local mac device
-            const response = await fetch('http://172.31.11.46:3000/login', {
+            const response = await fetch('${process.env.EXPO_PUBLIC_EMILIA_API_URL}/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
