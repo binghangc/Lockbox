4af9564dca303a37698564ba3fd43d14954393b4
Emilia Ayu <emiliaayu944@gmail.com>
2025-05-11 16:16:12 +0800

    Edited formatting issues.

--- src/components/editActionRow.tsx
+++ src/components/editActionRow.tsx
@@ -10,10 +10,10 @@ export default function EditActionRow({ onCancel, onSave }: EditActionRowProps) 
     return (
         <View className="flex-row justify-end space-x-4">
         <TouchableOpacity onPress={onCancel}>
-            <Text className="text-gray-400">Cancel</Text>
+            <Text className="text-gray-400">Cancel </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={onSave}>
-            <Text className="text-blue-400 font-semibold">Save</Text>
+            <Text className="text-blue-400 font-semibold"> Save</Text>
         </TouchableOpacity>
         </View>
     );
