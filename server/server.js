32964ddf77f677bc8fa12dc2cad39db55c250eed
Bing Hang <binghangc@gmail.com>
2025-05-15 19:20:14 +0800

    chore(server): initialize Express app and mount auth, profile, and trip APIs

--- server/server.js
+++ server/server.js
@@ -9,10 +9,7 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(cors());
 
-const supabase = createClient(process.env.EXPO_PUBLIC_SUPABASE_URL, process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY);
-
 // imported routes
-const uploadAvatarRoute = require('./routes/upload-avatar');
 const tripRoutes = require('./routes/trips');
 
 app.use(express.json());
@@ -28,7 +25,6 @@ app.use('/profile', require('./routes/profile'));
 
 // API trip endpoints: get and edit trip info, ie name, description, start_date, end_date
 app.use('/trips', tripRoutes);
-
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
