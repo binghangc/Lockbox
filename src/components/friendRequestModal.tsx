f69a0b765df726e96d8f2c8ce01c44b45f2b620f
Emilia Ayu <emiliaayu944@gmail.com>
2025-05-24 15:39:49 +0800

    feat(ui): center friend request modal and apply blur background

--- src/components/friendRequestModal.tsx
+++ src/components/friendRequestModal.tsx
@@ -1,5 +1,8 @@
 import { View, Text, Pressable, Animated } from "react-native";
+import Modal from "react-native-modal";
 import { FriendRequest } from "@/types";
+import FloatingAvatar from "@/components/floatingAvatar";
+import { BlurView } from 'expo-blur';;
 
 type Props = {
   visible: boolean;
@@ -21,13 +24,25 @@ export default function FriendRequestModal({
   if (!visible || !request) return null;
 
   return (
-    <View className="absolute top-0 left-0 right-0 bottom-0 justify-center items-center bg-black/50">
-        <Animated.View
-            style={{ transform: [{ scale: scaleAnim }] }}
-            className="w-72 h-72 bg-white rounded-2xl justify-center items-center px-4"
+    <Modal
+    isVisible={visible}
+    onBackdropPress={onClose}
+    animationIn="zoomIn"
+    animationOut="zoomOut"
+    backdropOpacity={0.6}
+    useNativeDriver
+  >
+        <BlurView
+            intensity={70}
+            tint="light"
+            className="rounded-2xl px-6 pt-10 pb-6 items-center overflow-visible bg-white/60"
         >
-        <Text className="text-black text-xl font-bold mb-2">{request.sender.username}</Text>
-        <Text className="text-black mb-4">{request.sender.bio || "No bio provided."}</Text>
+
+          <View className="items-center justify-center -mt-16 mb-6 relative">
+              {request.sender.avatar_url && <FloatingAvatar uri={request.sender.avatar_url} />}
+          </View>
+        <Text className="text-white text-xl font-bold mb-2">{request.sender.username}</Text>
+        <Text className="text-white mb-4">{request.sender.bio || "No bio provided."}</Text>
 
         <View className="flex-row gap-4 mb-4">
             <Pressable onPress={onAccept} className="bg-green-500 px-4 py-2 rounded-lg">
@@ -41,7 +56,7 @@ export default function FriendRequestModal({
         <Pressable onPress={onClose} className="bg-black px-4 py-2 rounded">
             <Text className="text-white">Close</Text>
         </Pressable>
-      </Animated.View>
-    </View>
+      </BlurView>
+      </Modal>
   );
 }
