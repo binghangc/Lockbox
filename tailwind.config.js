6d942139066adb601464087b9d9631a4cb4ce2c9
Bing Hang <binghangc@gmail.com>
2025-05-25 16:58:28 +0800

    feat(config): add expo-dev-client to enable development builds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  presets: [require('nativewind/preset')],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [
+    // eslint-disable-next-line global-require
+    require("nativewind/preset"),
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
