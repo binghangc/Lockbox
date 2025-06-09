215d457776173369d99dc70f54cf7210db3afab3
Bing Hang <binghangc@gmail.com>
2025-06-09 23:36:54 +0800

    chore(lint): TripsLayout

--- src/app/(protected)/trips/_layout.tsx
+++ src/app/(protected)/trips/_layout.tsx
@@ -3,6 +3,58 @@ import Octicons from '@expo/vector-icons/Octicons';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { BlurView } from 'expo-blur';
 
+import type { StackNavigationProp } from '@react-navigation/stack';
+import type { ParamListBase } from '@react-navigation/native';
+
+type HeaderLeftProps = {
+  navigation: StackNavigationProp<ParamListBase>;
+};
+
+function HeaderLeft({ navigation }: HeaderLeftProps) {
+  return (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Octicons
+        name="chevron-left"
+        size={28}
+        color="white"
+        style={{ marginLeft: 12 }}
+      />
+    </TouchableOpacity>
+  );
+}
+
+function HeaderLeftWrapper({
+  navigation,
+}: {
+  navigation: StackNavigationProp<ParamListBase>;
+}) {
+  return <HeaderLeft navigation={navigation} />;
+}
+
+function HeaderLeftWithNavigation({
+  navigation,
+}: {
+  navigation: StackNavigationProp<ParamListBase>;
+}) {
+  return <HeaderLeftWrapper navigation={navigation} />;
+}
+
+function HeaderBackground() {
+  return (
+    <BlurView intensity={60} tint="dark" style={StyleSheet.absoluteFill} />
+  );
+}
+
+function headerLeftWithNavigation({
+  navigation,
+}: {
+  navigation: StackNavigationProp<ParamListBase>;
+}) {
+  return <HeaderLeftWithNavigation navigation={navigation} />;
+}
+
+const headerBackground = () => <HeaderBackground />;
+
 export default function TripsLayout() {
   return (
     <Stack
@@ -11,23 +63,8 @@ export default function TripsLayout() {
         headerTransparent: true,
         headerTintColor: 'white',
         headerTitleAlign: 'center',
-        headerLeft: () => (
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Octicons
-              name="chevron-left"
-              size={28}
-              color="white"
-              style={{ marginLeft: 12 }}
-            />
-          </TouchableOpacity>
-        ),
-        headerBackground: () => (
-          <BlurView
-            intensity={60}
-            tint="dark"
-            style={StyleSheet.absoluteFill}
-          />
-        ),
+        headerLeft: headerLeftWithNavigation.bind(null, { navigation }),
+        headerBackground,
         title: '',
       })}
     />
