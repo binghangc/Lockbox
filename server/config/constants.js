b4894706ebb3dddaa7cffd153960d129c4a8785a
Emilia Ayu <emiliaayu944@gmail.com>
2025-06-15 11:36:24 +0800

    fix(auth): fix null DEFAULT_AVATAR_URL error.

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,4 +1,6 @@
 const DEFAULT_AVATAR_URL =
   'https://pub-dfbfcf454f1f4f498b66d614be763070.r2.dev/defaults/avatar-default.jpg';
 
-export default DEFAULT_AVATAR_URL;
+module.exports = {
+  DEFAULT_AVATAR_URL,
+};
