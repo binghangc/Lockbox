ad8073c8757dfd3ba0770abfe330f8cab8685dc4
Emilia Ayu <emiliaayu944@gmail.com>
2025-06-23 21:34:59 +0800

    chore(itinerary): moved handleShuffle logic into reshuffleVibecheck in useTodayVibecheck hook.

--- src/app/(protected)/trips/[tripId].tsx
+++ src/app/(protected)/trips/[tripId].tsx
@@ -19,7 +19,6 @@ import useTrips from '@/hooks/useTrips';
 import useTodayVibecheck from '@/hooks/useTodayVibecheck';
 import ParticipantRowList from '@/components/participantRowList';
 import VibecheckShuffleButton from '@/components/vibecheckShuffleButton';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const screenOptions = {
   headerTransparent: true,
@@ -46,7 +45,8 @@ export default function TripDetailScreen() {
   const insets = useSafeAreaInsets();
   const router = useRouter();
 
-  const { vibecheck, setVibecheck, vcloading } = useTodayVibecheck(tripIdStr);
+  const { vibecheck, reshuffleVibecheck, vcloading } =
+    useTodayVibecheck(tripIdStr);
 
   const HEADER_HEIGHT = insets.top + 60;
 
@@ -84,29 +84,6 @@ export default function TripDetailScreen() {
       console.log('Not implemented yet.');
     };
   }
-
-  const handleShuffle = async () => {
-    const token = await AsyncStorage.getItem('access_token');
-    const today = dayjs().format('YYYY-MM-DD');
-
-    try {
-      const res = await fetch(
-        `${process.env.EXPO_PUBLIC_API_URL}/trips/${tripIdStr}/vibecheck/${today}`,
-        {
-          method: 'PATCH',
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
-      const result = await res.json();
-
-      if (!res.ok) throw new Error(result.error);
-      setVibecheck(result.vibecheck);
-    } catch (err) {
-      console.error('Shuffle failed:', err.message);
-    }
-  };
 
   return (
     <View style={{ flex: 1, backgroundColor: 'transparent' }}>
@@ -231,7 +208,7 @@ export default function TripDetailScreen() {
         }
         bottomAccessory={
           isHost && trip.status === 'ongoing' ? (
-            <VibecheckShuffleButton onPress={handleShuffle} />
+            <VibecheckShuffleButton onPress={reshuffleVibecheck} />
           ) : null
         }
       />
--- src/hooks/useTodayVibecheck.ts
+++ src/hooks/useTodayVibecheck.ts
@@ -4,41 +4,55 @@ import dayjs from 'dayjs';
 
 export default function useTodayVibecheck(tripId: string) {
   const [vibecheck, setVibecheck] = useState<string | null>(null);
-  const [vcloading, setVCLoading] = useState(false);
+  const [vcloading, setLoading] = useState(true);
+
+  const today = dayjs().format('YYYY-MM-DD');
+
+  const fetchVibecheck = async () => {
+    setLoading(true);
+    const token = await AsyncStorage.getItem('access_token');
+    try {
+      const res = await fetch(
+        `${process.env.EXPO_PUBLIC_API_URL}/trips/${tripId}/vibecheck/${today}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      const result = await res.json();
+      if (!res.ok) throw new Error(result.error);
+      setVibecheck(result.vibecheck);
+    } catch (err) {
+      console.error('Error fetching vibecheck:', err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reshuffleVibecheck = async () => {
+    const token = await AsyncStorage.getItem('access_token');
+    try {
+      const res = await fetch(
+        `${process.env.EXPO_PUBLIC_API_URL}/trips/${tripId}/vibecheck/${today}`,
+        {
+          method: 'PATCH',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      const result = await res.json();
+      if (!res.ok) throw new Error(result.error);
+      setVibecheck(result.vibecheck);
+    } catch (err) {
+      console.error('Shuffle failed:', err.message);
+    }
+  };
 
   useEffect(() => {
-    if (!tripId) return;
-
-    const fetchVibecheck = async () => {
-      setVCLoading(true);
-      try {
-        const token = await AsyncStorage.getItem('access_token');
-        const today = dayjs().format('YYYY-MM-DD');
-
-        const res = await fetch(
-          `${process.env.EXPO_PUBLIC_API_URL}/trips/${tripId}/vibecheck/${today}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          },
-        );
-
-        const result = await res.json();
-        if (!res.ok) {
-          throw new Error(result.error || 'Failed to fetch vibecheck');
-        }
-
-        setVibecheck(result.vibecheck);
-      } catch (err) {
-        console.error('Failed to fetch today’s vibecheck:', err.message);
-      } finally {
-        setVCLoading(false);
-      }
-    };
-
     fetchVibecheck();
   }, [tripId]);
 
-  return { vibecheck, setVibecheck, vcloading };
+  return { vibecheck, vcloading, reshuffleVibecheck };
 }
