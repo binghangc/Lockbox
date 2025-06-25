a853da78e4c384e9f9d824091f866c915f08ae69
Emilia Ayu <emiliaayu944@gmail.com>
2025-06-25 13:53:52 +0800

    chore(test, trips): grouped trip tests under trips folder.

--- server/__tests__/trips/trips.participant.test.js
+++ server/__tests__/trips/trips.participant.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
-const app = require('../app.js');
-const supabaseAdmin = require('../utils/supabaseAdminClient.js');
-const deleteTestUsers = require('../utils/test/deleteTestUsers.js');
-const createTestUser = require('../utils/test/createTestUser.js');
+const app = require('../../app.js');
+const supabaseAdmin = require('../../utils/supabaseAdminClient.js');
+const deleteTestUsers = require('../../utils/test/deleteTestUsers.js');
+const createTestUser = require('../../utils/test/createTestUser.js');
 
 const EMAIL_PREFIXES = ['leave_trip', 'participants'];
 
