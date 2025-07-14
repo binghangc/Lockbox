0df860ef1460267826edb68212dc00519b27dfde
Emilia Ayu <emiliaayu944@gmail.com>
2025-07-14 19:43:35 +0800

    fix(rag): fixed unexpected require bug

--- server/rag/utils/embeddingClient.js
+++ server/rag/utils/embeddingClient.js
@@ -1,5 +1,6 @@
+const { GoogleGenerativeAIEmbeddings } = require('@langchain/google-genai');
+
 function createEmbeddingsClient() {
-  const { GoogleGenerativeAIEmbeddings } = require('@langchain/google-genai');
   const apiKey = process.env.GOOGLE_API_KEY;
   if (!apiKey) throw new Error('GOOGLE_API_KEY is not set');
 
