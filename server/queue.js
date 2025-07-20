db0f77d9e908efe67a661fbd3dd6d7e50ee79c24
Emilia Ayu <emiliaayu944@gmail.com>
2025-07-21 01:25:17 +0800

    test(redis): added mock queue without calling redis

--- server/queue.js
+++ server/queue.js
@@ -1,14 +1,27 @@
 const { Queue } = require('bullmq');
 const redis = require('./redis.js');
 
-const itineraryQueue = new Queue('embed-itinerary', { connection: redis });
-const vibechecksQueue = new Queue('embed-vibecheck', { connection: redis });
+const isTestEnv = process.env.NODE_ENV === 'test';
 
-[itineraryQueue, vibechecksQueue].forEach((queue) => {
-  queue.on('error', (err) => {
-    console.error(`[Queue Error] (${queue.name})`, err);
+const itineraryQueue = isTestEnv
+  ? require('./utils/test/mockQueue.js').mockQueue('embed-itinerary')
+  : new Queue('embed-itinerary', {
+      connection: redis,
+    });
+
+const vibechecksQueue = isTestEnv
+  ? require('./utils/test/mockQueue.js').mockQueue('embed-vibecheck')
+  : new Queue('embed-vibecheck', {
+      connection: redis,
+    });
+
+if (!isTestEnv) {
+  [itineraryQueue, vibechecksQueue].forEach((queue) => {
+    queue.on('error', (err) => {
+      console.error(`[Queue Error] (${queue.name})`, err);
+    });
   });
-});
+}
 
 module.exports = {
   itineraryQueue,
