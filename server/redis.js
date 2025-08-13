a894d1ca59bcb99170b3a6bb6ba4cdde7a24f358
Bing Hang <binghangc@gmail.com>
2025-08-13 23:25:54 +0800

    chore(redis): prod redis server

--- server/redis.js
+++ server/redis.js
@@ -1,14 +1,16 @@
-// const { Redis } = require('ioredis');
+const { Redis } = require('ioredis');
 
-// const redis = new Redis(process.env.REDIS_URL, {
-//   ...(process.env.REDIS_URL?.startsWith('rediss://') && { tls: {} }),
-//   maxRetriesPerRequest: null,
-// });
-// module.exports = redis;
+const redis = new Redis(process.env.REDIS_URL, {
+  ...(process.env.REDIS_URL?.startsWith('rediss://') && { tls: {} }),
+  maxRetriesPerRequest: null,
+});
+module.exports = redis;
 
+/*
 module.exports = {
   host: '127.0.0.1',
   port: 6379,
   // if using TLS:
   ...(process.env.REDIS_URL?.startsWith('rediss://') && { tls: {} }),
 };
+*/
